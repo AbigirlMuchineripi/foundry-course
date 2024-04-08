991e3619c8539fd6c2cb1002779117c041293915
Abigirl Muchineripi <muchineripiabby@gmail.com>
2024-04-08 15:01:02 +0200

    creating a contract within a contract

--- StorageFactoryNamedImports.sol
+++ StorageFactoryNamedImports.sol
@@ -2,16 +2,31 @@
 
 pragma solidity ^0.8.18;
 
+//creating a contract in a contract
+
 // Using imports contain this : import "./simpleStorage.sol"; 
 //Make use of named imports for cheap deployment
-import {SimpleStorage, SimpleStorage2} from "./simpleStorage.sol"; 
+import {SimpleStorage} from "./simpleStorage.sol"; 
 contract StorageFactory{
    
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
 
     function createSimpleStorageContract() public{
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
         
     }
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public{
+        // to interact with the contract you need
+        //Address
+        //ABI (application binary interface) (technically a lie, you need the function selector)
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
+    }
+
 } 
