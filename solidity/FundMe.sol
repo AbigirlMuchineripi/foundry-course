e410a9c54262aefba9c788fcbc3962dc7e424816
Abigirl Muchineripi <muchineripiabby@gmail.com>
2024-04-24 01:02:13 +0200

    fund and withdraw the contract

--- solidity/FundMe.sol
+++ solidity/FundMe.sol
@@ -19,6 +19,11 @@ contract FundMe{
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
     
+
+    address public owner;
+    constructor() {
+        owner = msg.sender;
+    }
     function fund() public payable{     
     require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough Eth"); // 1ETH = 1**18 Wei
     funders.push(msg.sender);
@@ -27,16 +32,16 @@ contract FundMe{
     //what is a revert?
     //undo any actions that have been done and send remaining gas back
 
-    }
-    
+    }    
      function withdraw() public{
+//require (msg.sender ==owner, "Must be owner!"); deleted to modifier
      //for loop
      //[1,2,3,4] elements
      // 0, 1,2,3 indexes
      // for(/*starting index, ending index, step amount*/ )
-     for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
-        address funder =funders[funderIndex];
-        addressToAmountFunded[funder] = 0;
+       for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+         address funder =funders[funderIndex];
+          addressToAmountFunded[funder] = 0;
      }
      //reset the array
      funders = new address[](0);
@@ -46,8 +51,8 @@ contract FundMe{
       //call
 
      //msg.sender = address
-     //payable(msg.sender) = payable address
-    
+     //payable(msg.sender) = payable  
+
      //transfer                                                   
      payable(msg.sender).transfer(address(this).balance);
      //send
@@ -55,12 +60,18 @@ contract FundMe{
      require(sendSuccess, "Send failed");
      //call
      (bool callSuccess, ) = payable (msg.sender).call{value:address(this).balance}("");
-     require(callSuccess, "Call failed");
+     require(callSuccess, "Call failed");   
      
-      
-
-     }
-  
-   
+     }  
+   //modifier
+   modifier  onlyOwner() {
+    require(msg.sender == owner, "Sender is not owner!");
+    _; //the require function is executed first then underscore executes the code in withdraw funct afterwards depending on the position of the _;
+         
+   }
 }
 
+// compile and deploy this contract
+//fund it
+// withdraw it using remix
+//and the owner account
