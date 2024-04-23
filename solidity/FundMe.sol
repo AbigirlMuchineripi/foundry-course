91b6e386c2777b777e0d4eeff36f9c99c602fe2b
Abigirl Muchineripi <muchineripiabby@gmail.com>
2024-04-23 14:18:33 +0200

    sending and withdrawing Ether

--- solidity/FundMe.sol
+++ solidity/FundMe.sol
@@ -22,15 +22,45 @@ contract FundMe{
     function fund() public payable{     
     require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough Eth"); // 1ETH = 1**18 Wei
     funders.push(msg.sender);
-    addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    addressToAmountFunded[msg.sender] += msg.value;
 
     //what is a revert?
     //undo any actions that have been done and send remaining gas back
 
     }
     
-   // function withdraw() public{}
+     function withdraw() public{
+     //for loop
+     //[1,2,3,4] elements
+     // 0, 1,2,3 indexes
+     // for(/*starting index, ending index, step amount*/ )
+     for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+        address funder =funders[funderIndex];
+        addressToAmountFunded[funder] = 0;
+     }
+     //reset the array
+     funders = new address[](0);
+     //withdraw the funds
+      //transfer
+      //send
+      //call
 
+     //msg.sender = address
+     //payable(msg.sender) = payable address
+    
+     //transfer                                                   
+     payable(msg.sender).transfer(address(this).balance);
+     //send
+     bool sendSuccess = payable(msg.sender).send(address(this).balance);
+     require(sendSuccess, "Send failed");
+     //call
+     (bool callSuccess, ) = payable (msg.sender).call{value:address(this).balance}("");
+     require(callSuccess, "Call failed");
+     
+      
+
+     }
+  
    
 }
 
