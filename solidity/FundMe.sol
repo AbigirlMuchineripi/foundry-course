f0c00a31593f5ffa09a2f1f5538638ead1bf56e4
Abigirl Muchineripi <muchineripiabby@gmail.com>
2024-04-24 02:30:26 +0200

    immutable, constant, revert for gas optimization

--- solidity/FundMe.sol
+++ solidity/FundMe.sol
@@ -10,22 +10,29 @@ pragma solidity ^0.8.18;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+//Gas Optimization
+//constant and immutable keywords to bring down the cost
+//without constant and immutable keywords gas will be higher
+//target the require keyword and revert
+
+error NotOwner();
+
 contract FundMe{
 
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18; //5*10**18
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
     
 
-    address public owner;
+    address public immutable i_owner;
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
     function fund() public payable{     
-    require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough Eth"); // 1ETH = 1**18 Wei
+    require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough Eth"); // 1ETH = 1**18 Wei
     funders.push(msg.sender);
     addressToAmountFunded[msg.sender] += msg.value;
 
@@ -60,18 +67,29 @@ contract FundMe{
      require(sendSuccess, "Send failed");
      //call
      (bool callSuccess, ) = payable (msg.sender).call{value:address(this).balance}("");
-     require(callSuccess, "Call failed");   
+     require(callSuccess, "Call failed");  
+     revert(); 
      
      }  
    //modifier
    modifier  onlyOwner() {
-    require(msg.sender == owner, "Sender is not owner!");
+    //require(msg.sender == i_owner, "Sender is not owner!");
+     if(msg.sender != i_owner)  {
+        revert NotOwner();
+     }
     _; //the require function is executed first then underscore executes the code in withdraw funct afterwards depending on the position of the _;
-         
+     
    }
+   //What happens if someone sends this contract Eth without calling fund function
+
+   //functions in solidity
+   //1. receive
+   //2. fallback
+
 }
 
 // compile and deploy this contract
 //fund it
 // withdraw it using remix
 //and the owner account
+// optimize gas
